ac76394af2a0427be41c98969b186af20a777081
kohei shimazaki <kohei.shimazaki.k@gmail.com>
2020-04-28 06:08:31 +0000

    投稿の時間表示機能追加

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -6,4 +6,19 @@ module BlogsHelper
       blog_path
     end
   end
+  def time_delta(updated_at)
+    time = (Time.now - updated_at).to_i
+    time_minute = 60
+    time_hour = time_minute * 60
+    time_day = time_hour * 24
+    if time < time_minute
+      return "#{time}s"
+    elsif time < time_hour
+      return "#{time / time_minute}m"
+    elsif time < time_day
+      return "#{time/ time_hour}h"
+    else
+      return l updated_at, format: :short
+    end
+  end
 end
